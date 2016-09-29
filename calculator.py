06a098d68136d3ac35d89d94e62c65877d43ebe4
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:37:54 -0700

    fixed errors and now allows for division with floats

--- calculator.py
+++ calculator.py
@@ -9,7 +9,11 @@ from arithmetic import *
 
 def get_number(tokens, index):
 	"""returns number for arithmetic from string input"""
-	return int(tokens[index])
+	if "." in tokens[index]:
+		return float(tokens[index])
+	else:
+		return int(tokens[index])
+
 
 def operator_type(tokens):
 	"""returns operator type for arithmetic from string input"""
@@ -18,29 +22,33 @@ def operator_type(tokens):
 def calc_shell():
 	"""Enter operator and equivalent number of integers to calculate answer. Press q to quit"""
 	while True:
-		calc_input = raw_input("Please enter a mathematical equation. ")
-		tokens = calc_input.split(' ')
-		if tokens[0] == "q":
-			break
-		num1 = get_number(tokens, 1)
-		if len(tokens) == 3:
-			num2 = get_number(tokens, 2)
-		operator = operator_type(tokens)
-		if operator == "+":
-			print add(num1, num2)
-		elif operator == "-":
-			print subtract(num1, num2)
-		elif operator == "*":
-			print multiply(num1, num2)
-		elif operator == "/":
-			print divide(num1, num2)
-		elif operator == "square":
-			print square(num1)
-		elif operator == "cube":
-			print cube(num1)
-		elif operator == 'pow':
-			print power(num1, num2)
-		elif operator == 'mod':
-			print mod(num1, num2)
+		try:
+			calc_input = raw_input("Please enter a mathematical equation. ").lower()
+			tokens = calc_input.split(' ')
+			if tokens[0] == "q":
+				break
+			num1 = get_number(tokens, 1)
+			if len(tokens) == 3:
+				num2 = get_number(tokens, 2)
+			operator = operator_type(tokens)
+			if operator == "+":
+				print add(num1, num2)
+			elif operator == "-":
+				print subtract(num1, num2)
+			elif operator == "*":
+				print multiply(num1, num2)
+			elif operator == "/":
+				print divide(num1, num2)
+			elif operator == "square":
+				print square(num1)
+			elif operator == "cube":
+				print cube(num1)
+			elif operator == 'pow':
+				print power(num1, num2)
+			elif operator == 'mod':
+				print mod(num1, num2)
+		except (ValueError, UnboundLocalError, IndexError):
+			print "That input was not valid. Please try again."
+
 
 calc_shell()
